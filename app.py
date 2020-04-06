12982eca769a7650dbb612cd0e4dc23c8cd0cf01
Ji Hua <michaelhua1993@foxmail.com>
2020-04-05 22:46:25 -0400

    add word cloud

--- app.py
+++ app.py
@@ -4,49 +4,95 @@ import datetime
 from PIL import Image
 import requests
 from io import BytesIO
+import jieba
+import os
+import matplotlib.pyplot as plt
+from imageio import imread
+from collections import Counter
+from wordcloud import WordCloud, ImageColorGenerator
 
-msg_file='message.js'
+d = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+msg_file = d + 'message.js'
 stinky_piggy_id = 'huaji1415'
 silly_piggy_id = 'wxid_2379733797521'
+user_dict = ['爱猪', '猪猪', '傻猪', '想猪', '傻猪', '傻猪猪', '臭猪', '笨猪', '小臭猪', '小仙女']
+for w in user_dict:
+    jieba.add_word(w)
 
-with open(msg_file) as f:
+stopword_file = d + '/stopwords-zh.txt'
+stopwords = []
+with open(stopword_file) as f:
+    stopword_text = f.read()
+    stopwords = stopword_text.split('\n')
+
+font_path = d + '/fonts/NotoSansSC-Light.otf'
+
+with open('message.js') as f:
     text = f.read()
 cleaned_text = text[11:] # remove variable declaration
 msg_json = json.loads(cleaned_text)
-# stinky_piggy_avatar_url = msg_json['member'][stinky_piggy_id]['head']
-# silly_piggy_avatar_url = msg_json['member'][silly_piggy_id]['head']
-
-stinky_piggy_avatar = Image.open('stinky_piggy.jpeg')
-silly_piggy_avatar = Image.open('silly_piggy.jpeg')
 messages = msg_json['message']
 
-def search(token, messages):
-    h_count, x_count = 0, 0
-    h_first, x_first = None, None
-    h_msg, x_msg = None, None
+def get_text(role):
+    text_chat = []
+    if role == 'stinky_piggy':
+        from_id = stinky_piggy_id
+    else:
+        from_id = silly_piggy_id
     for m in messages:
-        if token in m['m_nsContent'] and m['m_uiMessageType'] == 1:
-            if m['m_nsToUsr'] == 'huaji1415':
-                x_count += 1
-                if x_first is None:
-                    timestamp = m['m_uiCreateTime']
-                    x_first = datetime.datetime.fromtimestamp(timestamp)
-                    x_msg = m['m_nsContent']
-            else:
-                h_count += 1
-                if h_first is None:
-                    timestamp = m['m_uiCreateTime']
-                    h_first = datetime.datetime.fromtimestamp(timestamp)
-                    h_msg = m['m_nsContent']
-    return (h_count, x_count, h_first, x_first, h_msg, x_msg)
+        if m['m_uiMessageType'] == 1:
+            if m['m_nsFromUsr'] == from_id:
+                text_chat.append(m['m_nsContent'])
+    return text_chat
+
+def count_word(texts, stopwords=stopwords):
+    words = []
+    for text in texts:
+        tokens = jieba.lcut(text)
+        for token in tokens:
+            token = token
+            if token not in stopwords and token != '\n':
+                words.append(token)
+    return Counter(words)
+
+def generate_word_cloud_text(word_count, number=100):
+    cloud_words = []
+    for w in word_count.most_common(number):
+        cloud_words.append(w[0])
+    return ' '.join(cloud_words)
+
+def build_cloud(words, image_file='pig-head.jpg'):
+    back_coloring = imread(os.path.join(d, image_file))
+    wc = WordCloud(font_path=font_path, background_color="white", max_words=2000, mask=back_coloring,
+            max_font_size=100, random_state=42, width=1000, height=860, margin=2,)
+    wc.generate(cloud_words)
+    # image_colors_default = ImageColorGenerator(image_file)
+    return wc
 
 
-st.title('傻猪猪和臭猪猪的恋爱笔记')
-search_term = st.text_input('请输入要查找的词语', '猪猪')
-counts = search(search_term, messages)
-st.image(stinky_piggy_avatar, width=66)
-st.write(f"臭猪说了　{counts[0]}　次　{search_term}")
-st.write(f"第一次是在{counts[2]}   说的是　{counts[4]}")
-st.image(silly_piggy_avatar, width=66)
-st.write(f"傻猪说了　{counts[1]}　次　{search_term}")
-st.write(f"第一次是在{counts[3]}   说的是　{counts[5]}")
+st.title('傻猪猪和臭猪猪的聊天记录')
+role_cn = st.radio("你想看谁的聊天记录呢？",
+    ('傻猪猪', '臭猪猪'))
+if role_cn == '傻猪猪':
+    role = 'silly_piggy'
+else:
+    role = 'stinky_piggy'
+number = st.slider('请选择显示词数', 100, 250, 100, 25)
+image_file = st.selectbox('请选择一只猪猪的图片',
+    ('pig-head.jpg', 'piglet.jpg'))
+image = Image.open(image_file)
+st.image(image, caption='使用这张图作为基准', width=200)
+# uploaded_file = st.file_uploader("选择一张jpg图片", type="jpg")
+confirm = st.button('让我来看看')
+
+if confirm:
+    text_chat = get_text(role)
+    word_count = count_word(text_chat, stopwords)
+    cloud_words = generate_word_cloud_text(word_count, number)
+    wc = build_cloud(cloud_words, image_file)
+
+    plt.figure()
+    # recolor wordcloud and show
+    plt.imshow(wc, interpolation="bilinear")
+    plt.axis("off")
+    st.pyplot()
